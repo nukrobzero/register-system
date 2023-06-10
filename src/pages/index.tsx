9fbf22efe90aa4ea7fc6032663a29eae6da6ec5b
nukrobzero <nukrobzero@hotmail.com>
2023-06-10 09:31:04 +0700

    fix: text 1 email to many

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -201,7 +201,7 @@ export default function Home() {
                 <span className="md:ml-2 block md:inline text-red-600 text-xs">
                   สำหรับท่านใดที่จะใช้อีเมล์เดียวในการสมัครหลายคนทำได้ตามนี้
                   <span className="bg-blue-500 text-white px-1 md:p-1 ml-1 rounded-md">
-                    name+firstname1@example.com
+                    name+firstname@example.com
                   </span>
                 </span>
               </label>
